1167b70552b04eac9d789e1387a3e2edaf2c3865
LucasNarfgren <narfgren@hotmail.com>
2021-10-27 16:18:17 +0200

    Add project files.

---
+++ OOP Labb 1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_1
+{
+    public class Circle
+    {
+        public float Radie;
+        public float Pi = 3.141f;
+
+        public Circle (float radie)
+        {
+            this.Radie = radie;
+        }
+
+        public float GetArea(float Radie)
+        {
+            float CalcArea = Radie * Radie * Pi;
+            int Calc = Convert.ToInt32(CalcArea);
+            //return Calc;
+            return CalcArea;
+        }
+        public float GetCircumference(float Radie)
+        {
+            float CalcCircumference = Radie * 2 * Pi;
+
+            return CalcCircumference;
+        }
+    }
+}
---
+++ OOP Labb 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP_Labb_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle C1 = new Circle(0);
+            Console.WriteLine("Area av Cirkel 1: {0} ",C1.GetArea(5));
+
+            Console.WriteLine();
+
+            Circle C2 = new Circle(0);
+            Console.WriteLine("Area av Cirkel 2: {0}",C2.GetArea(6));
+            Console.WriteLine();
+            Circle C3 = new Circle(0);
+
+            Console.Write("Skriv in en Radie för att räkna ut en cirkels Omkrets: ");
+            Console.WriteLine("Omkretsen av Cirkel 3 är : {0}",C3.GetCircumference(float.Parse(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+    }
+}
